995c421ab892ddfdec2dd7b4dfad1b4af3f7ddeb
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2020-12-29 17:48:56 +0200

    Sorted monthNumb, Months + income

--- js/components/renderMetuBalansas.js
+++ js/components/renderMetuBalansas.js
@@ -1,3 +1,17 @@
+import { account } from "../data/data.js";
+
+function sortMonths(list) {
+    let sortedList = [];
+
+    sortedList = list.sort((a, b) => a.month - b.month);
+    return sortedList;
+}
+
+
+sortMonths(account);
+const sortedByMonths = sortMonths(account);
+console.log(sortedByMonths);
+
 function renderMetuBalansas(selector, account, months) {
     const DOM = document.querySelector(selector);
     if (!DOM) {
@@ -7,17 +21,17 @@ function renderMetuBalansas(selector, account, months) {
     let HTML = '';
 
     if (selector === '#content') {
-        for (let i=0; i<account.length; i++) {    
-            console.log(months);
-            HTML += `<div class="table-row">
-                    <div class="cell">${account[i].month}</div>
-                    <div class="cell">${months[i]}</div>
-                    <div class="cell">150.00 Eur</div>
-                    <div class="cell">-</div>
-                    <div class="cell">150.00 Eur</div>
-                </div>`
+for (let i=0; i<account.length; i++) {
+        HTML += `<div class="table-row">
+            <div class="cell">${account[i].month}</div>
+            <div class="cell">${months[i]}</div>
+            <div class="cell">${sortedByMonths[i].income} Eur</div>
+            <div class="cell">${account[1].expense} Eur</div>
+            <div class="cell"> Eur</div>
+        </div>`
         }
     }
+    
 DOM.innerHTML = HTML;
 
 
--- js/data/months.js
+++ js/data/months.js
@@ -1,3 +1,3 @@
 const months = ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis'];
-console.log(months);
+
 export { months }
